e0f29ded28de81e9eefbe1674496078f63a33368
elreefy14 <es-ahmedhossameldin2024@alexu.edu.eg>
2024-03-26 00:12:05 +0200

    Add new files and models

---
+++ lib/app/data/models/wilaya.dart
@@ -0,0 +1,25 @@
+class HouseModel {
+  final String distance;
+  final String name;
+  final String location;
+  final double price;
+  final int numberOfRooms; // Add this
+  final int numberOfBathrooms; // Add this
+  final String description;
+  final String ownerName;
+  final String ownerNumber;
+  final List<String> images;
+
+  HouseModel({
+    required this.distance,
+    required this.name,
+    required this.location,
+    required this.price,
+    required this.numberOfRooms, // Add this
+    required this.numberOfBathrooms, // Add this
+    required this.description,
+    required this.ownerName,
+    required this.ownerNumber,
+    required this.images,
+  });
+}
